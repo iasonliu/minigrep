9629c407a2ff30da634acaf96413272d2db6da1b
Xiangxu <iasonliu@gmail.com>
2021-10-27 18:38:57 +0800

    got args

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {} in filename: {}", query, filename);
 }
