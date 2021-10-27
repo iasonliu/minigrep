ebbd5425e7809850655b735fd74970ee69f4e191
Xiangxu <iasonliu@gmail.com>
2021-10-27 19:16:31 +0800

    fix file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,31 +11,8 @@ fn main() {
     });
 
     println!("Searching for {} in filename: {}", config.query, config.filename);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Appliction error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
 }
