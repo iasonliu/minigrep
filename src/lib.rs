354f67d7a77364e50e2038c05ced7b1acdb4f610
Xiangxu <iasonliu@gmail.com>
2021-10-28 14:51:25 +0800

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::{env, fs};
 use std::error::Error;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
@@ -9,7 +9,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-
         args.next();
         let query = match args.next() {
             Some(arg) => arg,
@@ -43,13 +42,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines()
+    contents
+        .lines()
         .filter(|line| line.contains(query))
         .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines()
+    contents
+        .lines()
         .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
 }
